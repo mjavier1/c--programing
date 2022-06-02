a70d5b5cda9bbf5195d207cde256bfce2b04d0a3
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-06-02 11:27:55 -0700

    Add files via upload

---
+++ array/Array/Array/Program.cs
@@ -0,0 +1,24 @@
+﻿//marcos 
+//programancion en udemy curso 
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num {1,1,1,1,} 
+            //igual funciona con el for que string 
+
+            String[] customers = { "mj", "aguirre", "Rodriguez", "26" };
+        //    Console.WriteLine(customers[2]);
+
+            foreach(String customer in customers)
+            {
+                Console.WriteLine(customer);
+
+            }
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
