dccffe1c746dcd1aecba4e642b5d35c8969590c8
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-06-06 19:37:47 -0700

    do loop

---
+++ dowhile/doloop/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace doloop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0;
+            do
+            {
+                Console.WriteLine("x");
+                i=i+1;
+            }while (i<10);
+
+         //   Console.WriteLine("Hello, World!");
+        }
+    }
+}
