d130ec6edce735e6d1886b78851e4c52e71889f3
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2022-06-11 15:31:33 -0700

    func

---
+++ voidfunc/Functions/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Functions
+{
+    internal class Program
+    {
+
+      /*//una func simple 
+        static void hello(String name)
+        {
+            Console.WriteLine("\nHello"+name);
+        }
+      */
+
+        static int sum(int x, int y) { 
+            //matematica 
+            
+            return x + y; }
+
+
+        static void Main(string[] args)
+        {
+            //   hello("udemy");  
+            // Console.WriteLine("Hello, World!");
+            int x = sum(3, 7);
+          
+            Console.WriteLine(x);
+             Console.ReadKey();
+        }
+      
+    }
+}
